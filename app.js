05575510f213b31cef653c33c05d12aa05c6855a
Arup <arupgorai.js@gmail.com>
2020-03-01 23:42:42 +0530

    fetched all products

---
+++ app.js
@@ -0,0 +1,59 @@
+// variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// cart
+let cart = [];
+
+// getting the products
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      return data.items.map(item => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const { url: image } = item.fields.image.fields.file;
+
+        return {
+          id,
+          title,
+          price,
+          image
+        };
+      });
+    } catch (err) {
+      console.log(new Error(err));
+    }
+  }
+}
+
+// display products
+class UI {
+  constructor() {}
+}
+
+// local storage
+class Storage {
+  constructor() {}
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+
+  // get all products
+  products
+    .getProducts()
+    .then(product => console.log("product =>", product))
+    .catch(err => new Error(err));
+});
