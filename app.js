bd9b1627eddbdcfcbe7196d175a386aefcdc4474
Arup <arupgorai.js@gmail.com>
2020-03-02 21:57:32 +0530

    added cart functionality dynamically

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const productsDOM = document.querySelector(".products-center");
 // cart
 let cart = [];
 
+// buttons
+let buttonsDOM = [];
+
 // getting the products
 class Products {
   async getProducts() {
@@ -66,7 +69,49 @@ class UI {
 
   getBagButtons() {
     const buttons = [...document.querySelectorAll(".bag-btn")];
-    console.log(buttons);
+    buttonsDOM = buttons;
+
+    buttons.forEach(button => {
+      let id = button.dataset.id;
+      let inCart = cart.find(item => item.id === id);
+
+      if (inCart) {
+        button.innerText = "In Cart";
+        button.disabled = true;
+      }
+      button.addEventListener("click", e => {
+        e.target.innerText = "In Cart";
+        e.target.disabled = true;
+
+        // get product from products
+        let cartItem = { ...Storage.getProduct(id), amount: 1 };
+
+        // add product to the cart
+        cart = [...cart, cartItem];
+
+        // save cart in localStorage
+        Storage.saveCart(cart);
+
+        // set cart values
+        this.setCartValues(cart);
+
+        // display cart item
+        // show the cart
+      });
+    });
+  }
+
+  setCartValues(cartArr) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+
+    cartArr.map(item => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
   }
 }
 
@@ -74,6 +119,15 @@ class UI {
 class Storage {
   static saveProducts(products) {
     localStorage.setItem("products", JSON.stringify(products));
+  }
+
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem("products"));
+    return products.find(product => product.id === id);
+  }
+
+  static saveCart(cartArr) {
+    localStorage.setItem("cart", JSON.stringify(cartArr));
   }
 }
 
